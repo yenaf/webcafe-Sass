0ebc5e40189cefca10909d18d2e6968b80f519cc
yena <spffhd0835@gmail.com>
2023-06-22 09:28:37 +0900

    .

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ const postcss = require("postcss");
 
 module.exports = {
   plugins: [
-    postcss([require("cssnano"), require("postcss-combine-media-query")]),
+    // postcss([require("cssnano"), require("postcss-combine-media-query")]),
+    postcss([ require("postcss-combine-media-query")]),
   ],
 };
